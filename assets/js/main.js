c7a0506a9a78e5e1d9c5fc08dc29de0de80ace35
karina palacios <karina.spalacios@gmail.com>
2017-06-17 00:01:21 -0400

    nuevo

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,30 @@ window.addEventListener('scroll', function () {
   }
 });
 
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
+
