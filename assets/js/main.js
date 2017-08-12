4847dd0e9ab6fc209846774e12fabad975cc258d
karina palacios <karina.spalacios@gmail.com>
2017-08-12 17:49:10 -0400

    finish 2.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ window.addEventListener('scroll', function () {
   const distanciaVertical = window.pageYOffset || document.documentElement.scrollTop,
   header = document.getElementById('header');
   
-  if (distanciaVertical > 60) {
+  if (distanciaVertical > 400) {
     header.classList.add("navbar-color");
   } else {
     header.classList.remove("navbar-color");
